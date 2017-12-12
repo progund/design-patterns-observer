ca2fa2b0e77baee6fba1bb3f7ddf5c65b6d7b121
Rikard Fröberg <rikard@morus.se>
2017-12-12 09:43:39 +0100

    Fixed code style

--- presentation-sources/ButtonObservers.java
+++ presentation-sources/ButtonObservers.java
@@ -2,56 +2,69 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-public class ButtonObservers{
+/**
+ * This class demonstrates the observer pattern, in that
+ * it shows how a JButton in Swing can have multiply action listenters.
+ */
+public class ButtonObservers {
   private JButton theButton;
   private JFrame frame;
-  
-  public ButtonObservers(){
+
+  /**
+   * Constructs a new ButtonObservers object,
+   * initializes its components, does the layout
+   * and shows the containing JFrame.
+   */
+  public ButtonObservers() {
     initComponents();
     layoutComponents();
     frame.setVisible(true);
   }
-  private void initComponents(){
-    frame = new JFrame("Layout changer");
+  
+  private void initComponents() {
+    frame = new JFrame("Multi-lingual Panic Button");
     frame.setLayout(new BorderLayout());
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     frame.setPreferredSize(new Dimension(500,500));
     theButton = new JButton("Hälärm");
     theButton.setFont(new Font("Arial", Font.PLAIN, 40));
     // Add a German panic button listener
-    theButton.addActionListener(new ActionListener(){
+    theButton.addActionListener(new ActionListener() {
         @Override
-        public void actionPerformed(ActionEvent ae){
+        public void actionPerformed(ActionEvent ae) {
           System.out.println("Hilfe!");
         }
       });
     // Add a Swedish panic button listener
-    theButton.addActionListener(new ActionListener(){
+    theButton.addActionListener(new ActionListener() {
         @Override
-        public void actionPerformed(ActionEvent ae){
+        public void actionPerformed(ActionEvent ae) {
           System.out.println("Hjälp!");
         }
       });
     // Add an English panic button listener
-    theButton.addActionListener(new ActionListener(){
+    theButton.addActionListener(new ActionListener() {
         @Override
-        public void actionPerformed(ActionEvent ae){
+        public void actionPerformed(ActionEvent ae) {
           System.out.println("Help!");
         }
       });
     // Add a Spanish panic button listener
-    theButton.addActionListener(new ActionListener(){
+    theButton.addActionListener(new ActionListener() {
         @Override
-        public void actionPerformed(ActionEvent ae){
+        public void actionPerformed(ActionEvent ae) {
           System.out.println("Socorro!");
         }
       });
   }
-  private void layoutComponents(){
+  
+  private void layoutComponents() {
     frame.add(theButton, BorderLayout.CENTER);
     frame.pack();
   }
-  public static void main(String[] args){
+
+  // Main creates an instance of ButtonObservers
+  public static void main(String[] args) {
     new ButtonObservers();
   }
 }
